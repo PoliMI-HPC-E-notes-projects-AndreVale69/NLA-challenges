92a059c991c9d7a9cd204304434ee74c6eafe124
AndreVale69 <sforzini31@gmail.com>
2024-10-15 17:39:07 +0200

    feat: add task 1

--- challenge-2/README.md
+++ challenge-2/README.md
@@ -8,8 +8,8 @@
 1. Load the image as an Eigen matrix with size $m \times n$.
    Each entry in the matrix corresponds to a pixel on the screen and takes a value somewhere between 0 
    (black) and 255 (white).
-   Compute the matrix product $A^{T}A$ and report the euclidean norm of $A^{T}A$.
+   Compute the matrix product $A^{T}A$ and report the Euclidean norm of $A^{T}A$.
    
-   Answer:
+   Answer: $1050410679.542489$
 
 [1]: https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/256px-Albert_Einstein_Head.jpg?20141125195928=&download=
--- challenge-2/src/main.cpp
+++ challenge-2/src/main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include "utils/matrix_utils.hpp"
 
+#include "utils/image_manipulation.hpp"
+#include "utils/matrix_utils.hpp"
+
 using namespace std;
 using namespace Eigen;
 using namespace matrix_utils;
@@ -16,5 +19,43 @@ using namespace matrix_utils;
 
 
 int main() {
+    /**********
+     * Task 1 *
+     **********/
+    // Load image from file
+    int width = 0, height = 0, channels = 0, row_offset = 0;
+    unsigned char* image_data;
+    try {
+        image_data = image_manipulation::load_image_from_file(
+            "../challenge-2/resources/Albert_Einstein_Head.jpg", width, height, channels
+        );
+    } catch ([[maybe_unused]] const std::runtime_error& e) {
+        image_data = image_manipulation::load_image_from_file(
+            "../resources/Albert_Einstein_Head.jpg", width, height, channels
+        );
+    }
+
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> einstein_img(height, width);
+    static MatrixXd einstein_matrix(height, width),
+                    einstein_matrix_transpose(height, width),
+                    einstein_matrix_transpose_times_matrix(height, width);
+    // Fill the matrices with image data
+    for (int i = 0; i < height; ++i) {
+        row_offset = i * width;
+        for (int j = 0; j < width; ++j) {
+            einstein_matrix(i, j) = static_cast<double>(image_data[row_offset + j]);
+        }
+    }
+    einstein_matrix_transpose = einstein_matrix.transpose();
+    einstein_matrix_transpose_times_matrix = einstein_matrix_transpose * einstein_matrix;
+
+    printf(
+    "\nTask 1. Load the image as an Eigen matrix with size m * n."
+        "Each entry in the matrix corresponds to a pixel on the screen and takes a value somewhere between 0"
+        "(black) and 255 (white)."
+        "Compute the matrix product A^{T}A and report the Euclidean norm of A^{T}A.\n"
+        "Answer: %f\n", einstein_matrix_transpose_times_matrix.norm()
+    );
+
     return 0;
 }
